fa6c833d29b8d01f833e49fbc61fa8c638a94ce5
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2014-10-13 16:57:04 -0400

    newLine bug fixed

--- AlignerBoost/src/net/sf/AlignerBoost/AlignerBoost.java
+++ AlignerBoost/src/net/sf/AlignerBoost/AlignerBoost.java
@@ -223,7 +223,7 @@ public class AlignerBoost {
 				"                  stats    mapped        add/update final mapped unique reads that passed all the filters" + newLine +
 //				"                  utils    sam2Loc       convert SAM/BAM files to AlignBoost tab-delimited alignment files" + newLine +
 				"                  utils    sam2Cover     convert SAM/BAM files to AlignBoost tab-delimited read-coverage files" + newLine +
-				"                  utils    sam2Wig       convert SAM/BAM files to UCSC Wiggle files" +
+				"                  utils    sam2Wig       convert SAM/BAM files to UCSC Wiggle files" + newLine +
 //				"                  utils    bed2Loc       convert UCSC bed files to AlignBoost tab-delimited alignment files" + newLine +
 //				"                  utils    bed2Cover     convert UCSC bed files to AlignBoost tab-delimited read-coverage files" + newLine +
 //				"                  utils    bed2Wig       convert UCSC bed files to UCSC Wiggle files" + newLine +
