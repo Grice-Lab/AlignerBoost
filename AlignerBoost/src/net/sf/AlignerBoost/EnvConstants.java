6fe75d04b62170ed42fde7f9c4304226fc40e0a2
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2014-11-06 09:56:46 -0500

    fixed program version string to v1.2

--- AlignerBoost/src/net/sf/AlignerBoost/EnvConstants.java
+++ AlignerBoost/src/net/sf/AlignerBoost/EnvConstants.java
@@ -12,9 +12,9 @@ package net.sf.AlignerBoost;
 public class EnvConstants {
 	// prog versions
 	public static final String progName = "AlignerBoost";
-	public static final String progVer = "v1.1";
+	public static final String progVer = "v1.2";
 	public static final String progDesc = "A tool for boosting the accuracy of NextGen-seq aligner";
-	public static final String progFile = "AlignerBoost-v1.1.jar";
+	public static final String progFile = "AlignerBoost-v1.2.jar";
 	// environment variables
 	public static final String newLine = System.getProperty("line.separator", "\n");
 	public static final String osName = System.getProperty("os.name");
