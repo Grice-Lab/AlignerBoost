81dcda2a23d851d46b253b2827ee671890a488d9
Qi Zheng <zhengqi@gricestation1.med.upenn.edu>
2014-11-11 20:36:53 -0500

    New class utils.Stats added

---
+++ AlignerBoost/src/net/sf/AlignerBoost/utils/Stats.java
@@ -0,0 +1,89 @@
+/**
+ * A utility class with simple Statistic methods
+ */
+package net.sf.AlignerBoost.utils;
+
+/**
+ * @author Qi Zheng
+ * @version 1.2
+ * @since 1.2
+ */
+public class Stats {
+
+	/** determine whether a string is a decimal integer
+	 * @return true if is a valid decimal integer
+	 */
+	public static boolean isDecimal(String s) {
+		if(s.isEmpty())
+			return false;
+		for(int i = 0; i < s.length(); i++) {
+			if(i == 0 && s.charAt(i) == '-') {
+				if(s.length() == 1)
+					return false;
+				else
+					continue;
+			}
+			if(!Character.isDigit(s.charAt(i)))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Transfer phred q-value to p-value
+	 * @param q  q-value in log10-scale
+	 * @param scale  phred scale
+	 * @return  p-value
+	 */
+	public static double phredQ2P(double q, double scale) {
+		if(q < 0)
+			q = 0;
+		return Math.pow(10.0, q / -scale);
+	}
+
+	/**
+	 * Transfer phred q-value to p-value in -10 scale
+	 * @param q  q-value in log10-scale
+	 * @return  p-value
+	 */
+	public static double phredQ2P(double q) {
+		return phredQ2P(q, PHRED_SCALE);
+	}
+
+	/**
+	 * Transfer phred p-value to q-value
+	 * @param p  p-value
+	 * @param scale  phred scale
+	 * @return  q-value in log10-scale
+	 */
+	public static double phredP2Q(double p, double scale) {
+		return -scale * Math.log10(p);
+	}
+
+	/**
+	 * Transfer phred p-value to q-value
+	 * @param p  p-value
+	 * @return  q-value in log10-scale
+	 */
+	public static double phredP2Q(double p) {
+		return phredP2Q(p, PHRED_SCALE);
+	}
+	
+	/**
+	 * Get the mean value of a byte array
+	 * @param array  byte array
+	 * @return  mean value in double
+	 */
+	public static double mean(byte[] array) {
+		if(array == null || array.length == 0)
+			return Double.NaN;
+		double x = 0;
+		for(byte b : array)
+			x += b;
+		return x / array.length;
+	}
+
+	public static final double PHRED_SCALE = 10; // scaling factor for phred scores
+	public static final int ASCII_OFFSET = 33;
+//	public static final int MAX_QUAL = 255; // max mapQ
+}
