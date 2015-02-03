ff452b7f5bdce0970129276438256c5332f594d1
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2015-02-03 09:49:33 -0500

    join Map implemented

--- AlignerBoost/src/net/sf/AlignerBoost/utils/StringUtils.java
+++ AlignerBoost/src/net/sf/AlignerBoost/utils/StringUtils.java
@@ -3,6 +3,8 @@
  */
 package net.sf.AlignerBoost.utils;
 
+import java.util.Map;
+
 /**
  * @author Qi Zheng
  * @version  v1.2
@@ -35,4 +37,27 @@ public class StringUtils {
 		return join(" ", arr);
 	}
 
+	/**
+	 * A generic method to join a Map of any objects using : and given separator
+	 * @param  map any map to be joined to string
+	 * @param  sep String separator
+	 * @return  joined string as "key:value[<sep>key:value]", or null if map is null
+	 */
+	public static <K, V> String join(Map<K, V> map, String sep) {
+		if(map == null)
+			return null;
+		StringBuilder str = new StringBuilder();
+		for(Map.Entry<K, V> entry : map.entrySet())
+			str.append(str.length() == 0 ? entry.getKey() + ":" + entry.getValue() : sep + entry.getKey() + ":" + entry.getValue());
+		return str.toString();
+	}
+
+	/**
+	 * A generic method to join a Map of any objects using : and default separator ','
+	 * @param  map any map to be joined to string
+	 * @return  joined string as "key:value[,key:value]", or null if map is null
+	 */
+	public static <K, V> String join(Map<K, V> map) {
+		return join(map, ",");
+	}
 }
