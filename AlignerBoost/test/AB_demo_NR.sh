0b72751aafb01894d049ab94a672500d7cdbfc66
Qi Zheng <e00011027@gmail.com>
2015-03-13 13:04:52 -0400

    test/ folder removed

--- AlignerBoost/test/AB_demo_NR.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-#java -jar -Xms 4G -Xmx 16G AlignerBooster-v1.1.jar run NR -readLen 101 -in /home/zhengqi/git/AlignerBoost/AlignerBoost/test/human_test1_trimmed.fastq -out /home/zhengqi/git/AlignerBoost/AlignerBoost/test/human_test1_NR.fas
