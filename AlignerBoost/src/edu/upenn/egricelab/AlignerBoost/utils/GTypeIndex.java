58b0b5abb73712ee01473b140dd965902a429541
Qi Zheng <zhengqi@upenn.edu>
2017-09-12 16:11:12 -0400

    BitMask enclosed type dependent GTypeIndex implementation

--- AlignerBoost/src/edu/upenn/egricelab/AlignerBoost/utils/GTypeIndex.java
+++ AlignerBoost/src/edu/upenn/egricelab/AlignerBoost/utils/GTypeIndex.java
@@ -12,67 +12,132 @@ import java.util.*;
  *
  */
 public class GTypeIndex {
+	/* embedded types and enums */
+	public static class BitMask {
+		/* constructors */
+		public BitMask(int length) {
+			this.length = length;
+			gtypeIdx = new HashMap<String, BitSet>(DEFAULT_NTYPES);
+		}
+		
+		/* member methods */
+		/** get length of this BitMask */
+		public int getLength() {
+			return length;
+		}
+		
+		/** get number of gtypes */
+		public int getNumGType() {
+			return gtypeIdx.size();
+		}
+		
+		/** add a new gtype into this BitMask, if not exists */
+		public void add(String gtype) {
+			if(!gtypeIdx.containsKey(gtype))
+				gtypeIdx.put(gtype, new BitSet(length));
+		}
+		
+		/** remove a gtype from this BitMask */
+		public void remove(String gtype) {
+			gtypeIdx.remove(gtype);
+		}
+		
+		/** get bit of given gtype at given location */
+		public boolean getBit(String gtype, int loc) {
+			return gtypeIdx.get(gtype).get(loc);
+		}
+		
+		/** set bit of given gtype at given location */
+		public void setBit(String gtype, int loc) {
+			gtypeIdx.get(gtype).set(loc);
+		}
+		
+		/**
+		 * set all bits of a given gtype in given genome region
+		 * @param gtype  gtype to be set
+		 * @param start  0-based start
+		 * @param end    1-based end
+		 */
+		public void setBit(String gtype, int start, int end) {
+			BitSet idx = gtypeIdx.get(gtype);
+			idx.set(start, end);
+		}
+		
+		/** unmask the gtypes matched at a given location */
+		public Set<String> unmask(int loc) {
+			Set<String> hits = new HashSet<String>(getNumGType());
+			for(Map.Entry<String, BitSet> pair : gtypeIdx.entrySet()) {
+				String gtype = pair.getKey();
+				BitSet idx = pair.getValue();
+				if(idx.get(loc))
+					hits.add(gtype);
+			}
+			return hits;
+		}
+		
+		/**
+		 * unmask the gtypes matched at a given genomic region
+		 * @param start  0-based start
+		 * @param end  1-based end
+		 * @return  a Set of matched gtypes
+		 */
+		public Set<String> unmask(int start, int end) {
+			Set<String> hits = new HashSet<String>(getNumGType());
+			for(Map.Entry<String, BitSet> pair : gtypeIdx.entrySet()) {
+				String gtype = pair.getKey();
+				BitSet idx = pair.getValue();
+				for(int i = start; i < end; i++)
+					if(idx.get(i)) {
+						hits.add(gtype); /* no search needed */
+						break;
+					}
+			}
+			return hits;
+		}
+		
+		/**
+		 * unmask the gtypes matched summary at a given genomic region
+		 * @param start  0-based start
+		 * @param end  1-based end
+		 * @return  a map of matching summary of gtypes
+		 */
+		public Map<String, Integer> unmaskSum(int start, int end) {
+			Map<String, Integer> hits = new HashMap<String, Integer>(getNumGType());
+			for(Map.Entry<String, BitSet> pair : gtypeIdx.entrySet()) {
+				int sum = pair.getValue().get(start, end).cardinality();
+				if(sum > 0)
+					hits.put(pair.getKey(), sum); /* only summarize significant gtypes */
+			}
+			return hits;
+		}
+		
+		/* member fields */
+		private int length;  /* length of this BitMask */
+		private Map<String, BitSet> gtypeIdx; /* per-gtype indices */
+		
+		/* class constants */
+		public static final int DEFAULT_NTYPES = 16;
+	}
+	
 	/* constructors */
 	/** Default constructor, initiate index with default size */
 	public GTypeIndex() {
-		bitShift = new ArrayList<String>(DEFAULT_NBIT);
-		chrIdx = new HashMap<String, BitSet>();
+		chrIdx = new HashMap<String, BitMask>();
 	}
 	
 	/* member methods */
-	/** get number of gtypes */
-	public int getNumGTypes() {
-		return bitShift.size();
-	}
-	
-	/** get shift for this gtype */
-	public int getShift(String gtype) {
-		return bitShift.indexOf(gtype);
-	}
-	
 	public int getChrLen(String chr) {
-		return chrIdx.get(chr).size() / maxBits;
+		return chrIdx.get(chr).getLength();
 	}
 	
-	/**
-	 * add a new Gtype with increasing shift, if not exists
-	 * @gtype  gtype to be added
-	 */
-	void addGType(String gtype) {
-		if(!bitShift.contains(gtype)) {
-			bitShift.add(gtype);
-			updateIndex();
-		}
-	}
-
 	/** add a new chrom with given length */
 	public void addChr(String chr, int len) {
-		chrIdx.put(chr, new BitSet(len * maxBits));
+		chrIdx.put(chr, new BitMask(len));
 	}
 	
 	/** remove a chrom from current index */
-	public BitSet removeChr(String chr) {
+	public BitMask removeChr(String chr) {
 		return chrIdx.remove(chr);
-	}
-	
-	/** update chrIdx if current number of gtypes exceeds the maxBits used in the index */
-	void updateIndex() {
-		if(bitShift.size() <= maxBits) /* no update necessary */
-			return;
-		int oldMaxBits = maxBits;
-		maxBits += (int) Math.ceil(maxBits * LOADING_FACTOR);
-		for(Map.Entry<String, BitSet> pair : chrIdx.entrySet()) {
-			String chr = pair.getKey();
-			BitSet oldIdx = pair.getValue();
-			int chrLen = getChrLen(chr);
-			/* create a new index */
-			BitSet idx = new BitSet(chrLen * maxBits);
-			/* copy the old index bits */
-			for(int i = 0; i < chrLen; i++)
-				for(int j = 0; j < oldMaxBits; j++)
-					idx.set(i *  maxBits + j, oldIdx.get(i * oldMaxBits + j));
-			pair.setValue(idx); /* update index */
-		}
 	}
 	
 	/**
@@ -83,106 +148,33 @@ public class GTypeIndex {
 	 * @param gtype  genetic type
 	 */
 	public void maskRegion(String chr, int start, int end, String gtype) {
-		addGType(gtype);
-		maskRegion(chr, start, end, getShift(gtype));
+		BitMask bitMask = chrIdx.get(chr);
+		bitMask.add(gtype);
+		bitMask.setBit(gtype, start, end);
 	}
 
-	/**
-	 * mask a given chrom region with given shift as checked
-	 * @param chr  chrom
-	 * @param start  0-based start
-	 * @param end  0-based end
-	 * @param shift  bit shift
-	 */
-	public void maskRegion(String chr, int start, int end, int shift) {
-		BitSet idx = chrIdx.get(chr);
-		for(int i = start; i <= end; i++)
-			maskLoc(idx, i, shift);
-	}
-	
-	/**
-	 * mask the index at given genome loc
-	 * @param idx  BitSet index
-	 * @param loc  0-based location
-	 * @param shift  bit shift
-	 */
-	public void maskLoc(BitSet idx, int loc, int shift) {
-		idx.set(loc * maxBits + shift);
-	}
-	
-	/**
-	 * get bit from given index at given genome location
-	 * @param idx
-	 * @param loc
-	 * @param shift
-	 * @return
-	 */
-	public boolean getLocBit(BitSet idx, int loc, int shift) {
-		return idx.get(loc * maxBits + shift);
-	}
-	
-	/**
-	 * get BitSet from a given genome region
-	 * @param chr  chrom
-	 * @param start  0-based start
-	 * @param end  0-based end
-	 * @return  a new BitSet representing this region
-	 */
-	public BitSet getRegionBitSet(String chr, int start, int end) {
-		int numBits = getNumGTypes();
-		BitSet idx = chrIdx.get(chr);
-		BitSet bits = new BitSet(numBits);
-		for(int i = start; i <= end; i++)
-			for(int j = 0; j < numBits; j++)
-				if(getLocBit(idx, i, j))
-					bits.set(j);
-		return bits;
-	}
-	
 	/**
 	 * unmask a given genome region
 	 * @param chr  chrom
 	 * @param start  0-based start
-	 * @param end  0-based end
-	 * @return  a set of matched gtypes
+	 * @param end  1-based end
+	 * @return  a set of matched gtypes, or null if this chrom does not exist
 	 */
 	public Set<String> unmask(String chr, int start, int end) {
-		int numBits = bitShift.size();
-		Set<String> hits = new HashSet<String>(numBits);
-		BitSet idx = chrIdx.get(chr);
-		for(int i = start; i <= end; i++)
-			for(int j = 0; j < numBits; j++)
-				if(getLocBit(idx, i, j))
-					hits.add(bitShift.get(j));
-		return hits;
+		return chrIdx.containsKey(chr) ? chrIdx.get(chr).unmask(start, end) : null;
 	}
 	
 	/**
 	 * unmask a given genome region and get the total matched length of each gtype
 	 * @param chr  chrom
 	 * @param start  0-based start
-	 * @param end  0-based end
-	 * @return  a map of matched lengths of each gtype
+	 * @param end  1-based end
+	 * @return  a map of matched lengths of each gtype, or null if this chrom does not exist
 	 */
 	public Map<String, Integer> unmaskSum(String chr, int start, int end) {
-		int numBits = bitShift.size();
-		Map<String, Integer> hitSum = new HashMap<String, Integer>(numBits);
-		BitSet idx = chrIdx.get(chr);
-		for(int i = start; i <= end; i++)
-			for(int j = 0; j < numBits; j++)
-				if(getLocBit(idx, i, j))
-					hitSum.put(bitShift.get(j), hitSum.getOrDefault(bitShift.get(j), 0) + 1);
-		return hitSum;
+		return chrIdx.containsKey(chr) ? chrIdx.get(chr).unmaskSum(start,  end) : null;
 	}
 	
 	/* member fields */
-	private Map<String, BitSet> chrIdx; /* per-chromosome indices */
-	private List<String> bitShift;
-	private int maxBits = DEFAULT_NBIT;
-	
-	/* class constants */
-	public static final int DEFAULT_NBIT = 16;
-	public static final double LOADING_FACTOR = 0.75;
-	
-	/* static utility methods */
+	private Map<String, BitMask> chrIdx; /* per-chromosome BitMask */
 }
